c7653ab1482a60d8d4bfb2d0a111c63f2fc02092
johnny <s154674@student.dtu.dk>
2022-06-23 11:03:38 +0200

    Secrets management

---
+++ scripts/modules/secrets.py
@@ -0,0 +1,14 @@
+import os
+import json
+
+
+def read_secrets() -> dict:
+    filename = os.path.join('../../secrets.json')
+    try:
+        with open(filename, mode='r') as f:
+            return json.loads(f.read())
+    except FileNotFoundError:
+        return {}
+
+
+secrets = read_secrets()
