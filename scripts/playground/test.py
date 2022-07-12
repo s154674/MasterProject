34d2b31eb6b6c343a0c86ffe2a9a013d1ce8ba9a
johnny <s154674@student.dtu.dk>
2022-07-12 14:01:32 +0200

    testing w3 connections and connector class

--- scripts/playground/test.py
+++ scripts/playground/test.py
@@ -1,3 +1,4 @@
+from configparser import SectionProxy
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -11,7 +12,9 @@ def get_abi(abi_name: str) -> dict:
         return load(f)['abi']
 
 
-w3 = Web3(Web3.HTTPProvider(f'http://{secrets["SERVER_IP"]}:{secrets["L1_RPC"]}'))
+# w3 = Web3(Web3.HTTPProvider(f'http://{secrets["SERVER_IP"]}:{secrets["L1_RPC"]}'))
+w3 = Web3(Web3.HTTPProvider(secrets["INFURA_URL"]))
+
 
 print(w3.isConnected())
 
@@ -23,4 +26,19 @@ print(UniSwapV2Factory.address, UniSwapV2Factory.abi, UniSwapV2Factory.functions
 print(UniSwapV2Factory.functions.allPairs(1).call())
 
 print(UniSwapV2Factory.functions.getPair("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2").call())
+
+arbi = Web3(Web3.HTTPProvider(secrets['ARBITRUM_URL']))
+
+print(arbi.isConnected())
+
+opti = Web3(Web3.HTTPProvider(secrets['OPTIMISM_URL']))
+
+print(opti.isConnected())
+
+
+networks = ["MAIN", "ARBI", "OPTI"]
+
+from modules.connector import Connector
+
+arbi = Connector('ARBI', arbi)
 
