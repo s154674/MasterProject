e280e6ce92aee8a1ff2fe4b6432b0154f8a2ac30
johnny <s154674@student.dtu.dk>
2022-07-15 13:29:04 +0200

    filed unique contraints and minor mods

--- MasterProject/events/models.py
+++ MasterProject/events/models.py
@@ -5,12 +5,13 @@ from django.db import models
 # Create your models here.
 class ERC20(models.Model):
     name = models.CharField(max_length=30, null=True)
-    symbol = models.CharField(max_length=10)
+    symbol = models.CharField(max_length=10, unique=True)
 
 
 class Networks(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
     chain_id = models.SmallIntegerField(null=True)
+    short = models.CharField(max_length=4, unique=True)
 
 
 class ERC20Addresses(models.Model):
@@ -18,6 +19,8 @@ class ERC20Addresses(models.Model):
     network = models.ForeignKey(Networks, on_delete=models.PROTECT)
     address = models.CharField(max_length=256)
 
+    class Meta:
+        unique_together = ["ERC20", "network"]
 
 class PoolAddresses(models.Model):
     network = models.ForeignKey(Networks, on_delete=models.PROTECT)
@@ -25,3 +28,6 @@ class PoolAddresses(models.Model):
     token1 = models.ForeignKey(ERC20, on_delete=models.PROTECT, related_name="token1")
     fee_tier = models.PositiveBigIntegerField()
     address = models.CharField(max_length=256)
+
+    class Meta:
+        unique_together = ["token0", "token1", "fee_tier", "network"]
