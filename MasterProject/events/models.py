196569903460b39e00d871d7842821a89175cb63
johnny <s154674@student.dtu.dk>
2022-07-16 16:25:01 +0200

    Fit bigger numbers

--- MasterProject/events/models.py
+++ MasterProject/events/models.py
@@ -3,6 +3,19 @@ from statistics import mode
 from unicodedata import name
 from django.db import models
 
+class BigIntegerField(models.IntegerField):
+    empty_strings_allowed=False
+    def get_internal_type(self):
+        return "BigIntegerField"	
+    def db_type(self, connection):
+        return 'bigint'
+class BiggerIntegerField(models.IntegerField):
+    empty_strings_allowed=False
+    def get_internal_type(self):
+        return "BiggerIntegerField"	
+    def db_type(self, connection):
+        return 'NUMERIC(77)'
+
 # Create your models here.
 class ERC20(models.Model):
     name = models.CharField(max_length=30, null=True)
@@ -12,7 +25,7 @@ class ERC20(models.Model):
 
 class Networks(models.Model):
     name = models.CharField(max_length=20, unique=True)
-    chain_id = models.SmallIntegerField(null=True)
+    chain_id = models.IntegerField(null=True)
     short = models.CharField(max_length=4, unique=True)
 
 
@@ -34,11 +47,12 @@ class PoolAddresses(models.Model):
     class Meta:
         unique_together = ["token0", "token1", "fee_tier", "network"]
 class SwapEvent(models.Model):
+    pool_address = models.ForeignKey(PoolAddresses, on_delete=models.PROTECT)
     event_hash = models.CharField(max_length=256)
     sender = models.CharField(max_length=256)
     recipient = models.CharField(max_length=256)
-    amount0 = models.CharField(max_length=256)
-    amount1 = models.CharField(max_length=256)
-    sqrtPriceX96 = models.CharField(max_length=256)
-    liquidity = models.CharField(max_length=256)
-    tick = models.CharField(max_length=256)
+    amount0 = models.DecimalField(max_digits=77, decimal_places=0)
+    amount1 = models.DecimalField(max_digits=77, decimal_places=0)
+    sqrtPriceX96 = models.CharField(max_length=100)
+    liquidity = models.DecimalField(max_digits=77, decimal_places=0)
+    tick = models.DecimalField(max_digits=77, decimal_places=0)
