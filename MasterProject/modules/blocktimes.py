a2242bdedf1640cf8c776234e69d1c459de7cd2a
johnny <s154674@student.dtu.dk>
2022-07-19 00:33:13 +0200

    Fix typo

--- MasterProject/modules/blocktimes.py
+++ MasterProject/modules/blocktimes.py
@@ -2,20 +2,20 @@ from enum import Enum
 
 class MainBlocks(Enum):
     # All block between the 1st and 7th (inclusive) of july 2022
-    FRIST_WEEK_JULY = (15053226, 15098519) 
+    FIRST_WEEK_JULY = (15053226, 15098519) 
     JUNE = (14881650, 15053225)
 
 class OptiBlocks(Enum):
     # All block between the 1st and 7th (inclusive) of july 2022
-    FRIST_WEEK_JULY = (86417, 89163) 
+    FIRST_WEEK_JULY = (86417, 89163) 
     JUNE = (63340, 86416)
 
 class ArbiBlocks(Enum):
     # All block between the 1st and 7th (inclusive) of july 2022
-    FRIST_WEEK_JULY = (16662000, 17220200) 
+    FIRST_WEEK_JULY = (16662000, 17220200) 
     JUNE = (13383500, 16661999)
 
 class PolyBlocks(Enum):
     # All block between the 1st and 7th (inclusive) of july 2022
-    FRIST_WEEK_JULY = (30194060, 30462400)
+    FIRST_WEEK_JULY = (30194060, 30462400)
     JUNE = (29011700, 30194059)
