06b9b12d77af62e4f5a6e9509160d1b807b63a2b
johnny <s154674@student.dtu.dk>
2022-07-20 01:12:56 +0200

    Make all range

--- MasterProject/modules/blocktimes.py
+++ MasterProject/modules/blocktimes.py
@@ -4,18 +4,22 @@ class MainBlocks(Enum):
     # All block between the 1st and 7th (inclusive) of july 2022
     FIRST_WEEK_JULY = (15053226, 15098519) 
     JUNE = (14881650, 15053225)
+    ALL = (14881650, 15098519)
 
 class OptiBlocks(Enum):
     # All block between the 1st and 7th (inclusive) of july 2022
     FIRST_WEEK_JULY = (86417, 89163) 
     JUNE = (63340, 86416)
+    ALL = (63340, 89163)
 
 class ArbiBlocks(Enum):
     # All block between the 1st and 7th (inclusive) of july 2022
     FIRST_WEEK_JULY = (16662000, 17220200) 
     JUNE = (13383500, 16661999)
+    ALL = (13383500, 17220200)
 
 class PolyBlocks(Enum):
     # All block between the 1st and 7th (inclusive) of july 2022
     FIRST_WEEK_JULY = (30194060, 30462400)
     JUNE = (29011700, 30194059)
+    ALL = (29011700, 30462400)
