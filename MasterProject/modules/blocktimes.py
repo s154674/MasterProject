24ded11c7aece7b5a8d33046a88ea16f9146f62d
johnny <s154674@student.dtu.dk>
2022-07-22 00:20:49 +0200

    backup

--- MasterProject/modules/blocktimes.py
+++ MasterProject/modules/blocktimes.py
@@ -1,4 +1,9 @@
 from enum import Enum
+from events.models import Networks, BlockTimes
+import numpy as np
+from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
+from numpy.polynomial import Polynomial as P
 
 class MainBlocks(Enum):
     # All block between the 1st and 7th (inclusive) of july 2022
@@ -23,3 +28,36 @@ class PolyBlocks(Enum):
     FIRST_WEEK_JULY = (30194060, 30462400)
     JUNE = (29011700, 30194059)
     ALL = (29011700, 30462400)
+
+
+class Blocktimes():
+    def __init__(self, network):
+        x = []
+        y = []
+        blocks = BlockTimes.objects.filter(network=network)
+        for block in blocks:
+            x.append(block.blockNumber)
+            y.append(int(block.timestamp))
+        # print(x,y)
+        self.model = np.poly1d(np.polyfit(x,y,2))
+        
+    
+        # print(r2_score(y, model(x)))
+
+    def get_blocktime(self, x):
+        return int(self.model(x))
+
+    def expand_timestamp(self, ts):
+        lower = ts - 30*60
+        upper = ts + 30*60
+        
+            
+    def get_range(self, block, minutes=30):
+        ts = int(self.model(block))
+        lower = ts - minutes*60
+        upper = ts + minutes*60
+        return (lower, upper)
+
+    def test(self, number):
+        p = P.fit(self.model)
+        return (p - number).roots()
